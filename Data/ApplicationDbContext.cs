d74a634a527bddeda5bf6bd95487f5e369bf878d
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-07-21 09:56:12 +0200

    Create Data file

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Grimoire_Csharp.Models;
+
+namespace Grimoire_Csharp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
